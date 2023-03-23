14cdc5f78bd5a5b942ab3238535b6519c7bdd13f
KC <kcjones.2002@gmail.com>
2023-03-22 23:39:12 -0700

    api: make use of browser AbortController

--- api/api.js
+++ api/api.js
@@ -9,18 +9,24 @@ const getCookie = (name) => {
     }, '')
 }
 
-function checkForServerErr(response) {
-    if(typeof(response.status) === "string" && response.status.toLowerCase() === "error") { throw response; }
+function checkForServerErr(response, signal) {
+    if(typeof(response.status) === "string" && response.status.toLowerCase() === "error") { 
+        controllers = controllers.filter((controller) => controller.signal !== signal)
+        throw response;
+    }
     return response;
 }
 
 function handlerErr(type, err) {
-    events.trigger("error", {type: type, err: err})
-    console.error("From:", type, "Err:", err);
+    if (err.name === 'AbortError') { console.log(`Api aborted fetch`) }
+    else {
+        events.trigger("error", {type: type, err: err})
+        console.error("From:", type, "Err:", err);
+    }
 }
 
 window.HOST = location.protocol+"//"+location.host;
-
+let controllers = []
 
 // TODO: Check the return status of our call. If we get a 404 try again X times
 //   until we either get a response or hit the retry limit.
@@ -29,14 +35,26 @@ window.HOST = location.protocol+"//"+location.host;
 
 const api = {
 
+    abortAllRequests: function() {
+        controllers = controllers.filter((controller) => {
+            controller.abort()
+            return !controller.signal.aborted
+        })
+    },
+
     get: function(type, opts, callback) {
         if(typeof(opts) === "function") { callback = opts; opts = {} }
         if(type.charAt(0) !== "/") { type = `/${type}`; }
         opts.type = type
         let returnAs = opts.returnAs || "json"
 
+        const abortcontroller = new AbortController();
+        const signal = abortcontroller.signal;
+        controllers.push(abortcontroller)
+
         let request = {
             method: "GET",
+            signal: signal,
             headers: {
                 "Auth-Email": getCookie("Auth-Email"),
                 "Auth-Key": getCookie("Auth-Key"),
@@ -49,8 +67,11 @@ const api = {
             if(returnAs === "json") { return r.json() }
             return r.json()
         })
-        .then(checkForServerErr)
-        .then(callback)
+        .then((res) => checkForServerErr(res, signal))
+        .then((res) => {
+            controllers = controllers.filter((controller) => controller.signal !== signal)
+            callback(res)
+        })
         .catch((err) => { handlerErr(type, err) })
     },
 
@@ -58,8 +79,14 @@ const api = {
         if(typeof(opts) === "function") { callback = opts; opts = {} }
         if(type.charAt(0) !== "/") { type = `/${type}`; }
         opts.type = type
+
+        const abortcontroller = new AbortController();
+        const signal = abortcontroller.signal;
+        controllers.push(abortcontroller)
+
         let request = {
             method: "POST",
+            signal: signal,
             body: JSON.stringify(opts),
             headers: {
                 "Auth-Email": getCookie("Auth-Email"),
@@ -68,8 +95,11 @@ const api = {
         }
         fetch(`${HOST}/api/post`+type, request)
         .then((r) => r.json())
-        .then(checkForServerErr)
-        .then(callback)
+        .then((res) => checkForServerErr(res, signal))
+        .then((res) => {
+            controllers = controllers.filter((controller) => controller.signal !== signal)
+            callback(res)
+        })
         .catch((err) => { handlerErr(type, err) })
     },
 
@@ -77,8 +107,14 @@ const api = {
         if(typeof(opts) === "function") { callback = opts; opts = {} }
         if(type.charAt(0) !== "/") { type = `/${type}`; }
         opts.type = type
+
+        const abortcontroller = new AbortController();
+        const signal = abortcontroller.signal;
+        controllers.push(abortcontroller)
+
         let request = {
             method: "PUT",
+            signal: signal,
             body: JSON.stringify(opts),
             headers: {
                 "Auth-Email": getCookie("Auth-Email"),
@@ -87,8 +123,11 @@ const api = {
         }
         fetch(`${HOST}/api/put`+type, request)
         .then((r) => r.json())
-        .then(checkForServerErr)
-        .then(callback)
+        .then((res) => checkForServerErr(res, signal))
+        .then((res) => {
+            controllers = controllers.filter((controller) => controller.signal !== signal)
+            callback(res)
+        })
         .catch((err) => { handlerErr(type, err) })
     },
 }
